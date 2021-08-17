b0967df2f0eeffa877c4a1add7ce544af5a58996
mehdigoom <medhi.gay@sfr.fr>
2021-08-17 15:02:37 +0200

    Import ok

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 
 const express = require('express')
-const fs = require('fs'); // Inclusion de fs (fs.readfile)
+const fs = require('fs'); 
 const path = require("path");
 const app = express()
 const port = 80
 
+
+
+
 app.get('/', (req, res) => {
+
+    
     res.sendFile(path.join( __dirname, "Pages/index.html"));
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
