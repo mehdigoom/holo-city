0d8fc0ce5e4ea4764775fa0225c1f41954893039
mehdigoom <medhi.gay@sfr.fr>
2021-08-18 13:57:20 +0200

    Purge Index.html + import Lucy.glb and libs for 3D model

--- index.js
+++ index.js
@@ -5,16 +5,14 @@ const path = require("path");
 const app = express()
 const port = 80
 
-
-
+//////////------------------------ ALL PAGES---------------------------------------/////
 
 app.get('/', (req, res) => {
 
     
     res.sendFile(path.join( __dirname, "Pages/index.html"));
 })
-
-
+//////////------------------------ ALL JS ---------------------------------------/////
 app.get('/three.js', (req, res) => {
 
     
@@ -27,8 +25,22 @@ app.get('/main.js', (req, res) => {
     res.sendFile(path.join( __dirname, "Pages/JS/main.js"));
 })
 
+app.get('/GLTFLoader.js', (req, res) => {
+    
+    
+    res.sendFile(path.join( __dirname, "Pages/JS/examples/jsm/loaders/GLTFLoader.js"));
+})
+//////////------------------------ ALL MODELS ---------------------------------------/////
+
+
+app.get('Lucy.glb', (req, res) => {
+    
+    
+    res.sendFile(path.join( __dirname, "Pages/Models/Lucy.glb"));
+})
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`listening at http://localhost:${port}`)
 })
 
