fe837532bc4f5e4d657d0e6964683a91b9266d27
mehdigoom <medhi.gay@sfr.fr>
2021-08-23 10:23:54 +0200

    Fix link and include Main.js

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,18 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.131.3';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const size = 10;
+const divisions = 10;
+
+const gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper );
 
 
 
+
+
+renderer.render( scene, camera );
